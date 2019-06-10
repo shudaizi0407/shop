8353e2007ffa0ba69924fc9fa57437de05cde53d
shudaizi0407 <1119028842@qq.com>
2019-06-10 10:27:23 +0800

    第一次添加

---
+++ resources/views/admin/index/common/aside.blade.php
@@ -0,0 +1,53 @@
+<aside class="lt_aside_nav content mCustomScrollbar">
+ <h2><a href="index.html">起始页</a></h2>
+ <ul>
+  <li>
+   <dl>
+    <dt>常用布局示例</dt>
+    <!--当前链接则添加class:active-->
+    <dd><a href="product_list.html" class="active">商品列表示例</a></dd>
+    <dd><a href="product_detail.html">商品详情示例</a></dd>
+    <dd><a href="recycle_bin.html">商品回收站示例</a></dd>
+   </dl>
+  </li>
+  <li>
+   <dl>
+    <dt>订单信息</dt>
+    <dd><a href="order_list.html">订单列表示例</a></dd>
+    <dd><a href="order_detail.html">订单详情示例</a></dd>
+   </dl>
+  </li>
+  <li>
+   <dl>
+    <dt>会员管理</dt>
+    <dd><a href="user_list.html">会员列表示例</a></dd>
+    <dd><a href="user_detail.html">添加会员（详情）示例</a></dd>
+    <dd><a href="user_rank.html">会员等级示例</a></dd>
+    <dd><a href="adjust_funding.html">会员资金管理示例</a></dd>
+   </dl>
+  </li>
+  <li>
+   <dl>
+    <dt>基础设置</dt>
+    <dd><a href="setting.html">站点基础设置示例</a></dd>
+   </dl>
+  </li>
+  <li>
+   <dl>
+    <dt>配送与支付设置</dt>
+    <dd><a href="express_list.html">配送方式</a></dd>
+    <dd><a href="pay_list.html">支付方式</a></dd>
+   </dl>
+  </li>
+  <li>
+   <dl>
+    <dt>在线统计</dt>
+    <dd><a href="discharge_statistic.html">流量统计</a></dd>
+    <dd><a href="sales_volume.html">销售额统计</a></dd>
+   </dl>
+  </li>
+  <li>
+   <p class="btm_infor">© DeathGhost.cn 版权所有</p>
+  </li>
+ </ul>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace'=>'Admin' ],function(){
+	Route::get('index','IndexController@index');
+});
